0d5485ea356a91456d39843b68f3be7857f79641
Duc Pho <ducpho@plumvillage.org>
2021-12-22 18:32:03 +0100

    Ignore node_modules
    - install package locally
    - fix sass --watch command

--- development.sh
+++ development.sh
@@ -1,6 +1,2 @@
-sass --watch style.sass css/compiled.css
-
-sudo npm install --global showdown
-npm link showdown
-
-sudo npm install --global @11ty/eleventy
+npm install
+sass --watch style.sass:css/compiled.css
