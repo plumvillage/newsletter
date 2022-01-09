b2330981b19947cfa1b6d84a780195d0ce229d7f
Duc Pho <ducpho@plumvillage.org>
2022-01-09 20:23:52 +0100

    Change directory structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,19 @@
-module.exports = function(eleventyConfig) {
-  // Output directory: _site
+module.exports = function (eleventyConfig) {
+    // Output directory: _site
 
-  // Copy `assets/` to `_site/assets`
-  eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addWatchTarget("assets");
-  
-  // Copy any .jpg file to `_site`, via Glob pattern (in 0.9.0+)
-  // Keeps the same directory structure.
-  eleventyConfig.addPassthroughCopy("**/*.jpg");
+    // Copy `assets/` to `_site/assets`
+    eleventyConfig.addPassthroughCopy("src/pagedjs");
+    eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addWatchTarget("src/css");
+
+    // Copy any .jpg file to `_site`, via Glob pattern (in 0.9.0+)
+    // Keeps the same directory structure.
+    eleventyConfig.addPassthroughCopy("**/*.jpg");
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        },
+    }
 };
