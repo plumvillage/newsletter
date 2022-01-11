8c40a7599e4ee9211629e013586682550b7fa0b1
rbyte <matthias.graf@mgrf.de>
2022-01-11 18:14:49 +0100

    fitting cover into bleed

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,11 @@
 module.exports = function (eleventyConfig) {
-    // Output directory: _site
-
-    // Copy `assets/` to `_site/assets`
     eleventyConfig.addPassthroughCopy("src/pagedjs");
     eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy({ "src/media/publish": "media" });
     eleventyConfig.addWatchTarget("src/css");
-
+    
     // Copy any .jpg file to `_site`, via Glob pattern (in 0.9.0+)
     // Keeps the same directory structure.
-    eleventyConfig.addPassthroughCopy("**/*.jpg");
     eleventyConfig.addPassthroughCopy("src/CNAME");
 
     // Create localized collections of articles
@@ -18,7 +15,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addCollection("articles_vi", function (collection) {
         return collection.getFilteredByGlob("./src/vi/articles/*.md");
     });
-
 
     return {
         dir: {
