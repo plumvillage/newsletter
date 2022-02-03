c5426942ba1f4c2c44957c158820efb4b4df39aa
rbyte <matthias.graf@mgrf.de>
2022-02-03 14:43:54 +0100

    building article pdfs automatically

---
+++ src/en/articles/articles.11tydata.js
@@ -0,0 +1,16 @@
+// https://www.11ty.dev/docs/data-template-dir/
+module.exports = {
+  pagination: {
+    data: "builds",
+    size: 1
+  },
+  builds: ["articles", "articles-print"],
+  permalink: "/{{ locale }}/{{ builds[pagination.pageNumber] }}/{{ page.fileSlug }}/",
+  layout: "layouts/article",
+  subLayout: ["layouts/web.njk", "layouts/print-frame.njk"],
+  locale: "en",
+  size: "A4",
+  bleed: "0mm",
+  marks: "none",
+  preview: true
+}
