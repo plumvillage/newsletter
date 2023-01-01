2c105558aea608db25986b5f6424bf1bc3a02cbd
rbyte <matthias.graf@mgrf.de>
2023-01-01 15:24:28 +0100

    cont.

--- src/_data/article-order-en-2023.js
+++ src/_data/article-order-en-2023.js
@@ -1,3 +1,8 @@
 module.exports = [
-"sr-chan-duc--interview",
+"su-ong--let-us-share-the-vision",
+"sr-chan-duc--reflections-on-engaged-and-applied-buddhism",
+"sr-the-nghiem--a-true-teacher",
+"br-phap-luu--thay-in-my-heart",
+"sr-dao-nghiem--my-journey-with-thay",
+"ha-vinh-tho--meeting-my-teacher",
 ]
